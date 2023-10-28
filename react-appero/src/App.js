9870e117bd14e991603e9aab4e84d9761bd4ee9a
Trotta Rosario <appero.app@gmail.com>
2023-10-28 18:45:25 +0200

    bootstrap

--- react-appero/src/App.js
+++ react-appero/src/App.js
@@ -1,8 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <div className="App">
-     <h1>TEST</h1>
+    <div className="container-fluid">
+      <h1>TEST</h1>
+      <button className="btn btn-primary">Click me</button>
     </div>
   );
 }
